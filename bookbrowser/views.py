d1ca765921490cd79083fd08b7c2c9f9461f99d3
Jacek <trojanowski.jacek@gmail.com>
2020-07-09 15:44:08 +0200

    hide secret key

--- bookbrowser/views.py
+++ bookbrowser/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from bookbrowser.models import Book
 
-# Create your views here.
+
+class BookList(ListView):
+    context_object_name = 'book_list'
+    # queryset = Book.objects.filter(publisher__name='ACME Publishing')
+    # template_name = 'books/acme_list.html'
+
+
+class NewBook(CreateView):
+    pass
--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jhrv($+8gha!3(q%h*gcxb$q$nxhi6u=z4ei@q7c)7addhpri%'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
